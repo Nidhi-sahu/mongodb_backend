42f9635bcb68601a839ac9a205ef312324345682
Nidhi7067 <nidhi@kriscent.in>
2024-08-05 07:30:11 -0700

    first commit

--- index.js
+++ index.js
@@ -6,10 +6,32 @@ app.use(cors())
 app.use(express.json())
 const UserModel = require('./Models/Users')
 const userdata = require('./Routes/UsersRoute')
+require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/tnplab")
 
-app.use('/',userdata);
-app.listen(3000, () =>{
-    console.log("on port 3000")
+// mongoose.connect("mongodb://127.0.0.1:27017/tnplab")
+
+
+// app.use('/',userdata);
+// app.listen(3000, () =>{
+//     console.log("on port 3000")
+// })
+
+;
+
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+    .then(() => console.log('Connected to the database'))
+    .catch((err) => console.error('Failed to connect to the database:', err));
+
+
+app.use('/', userdata);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
